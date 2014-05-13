17f6749518a4aaa31dabf1470993578bb1bd6b32
Walter Vargas <walter@covetel.com.ve>
2014-05-13 10:55:15 -0430

    setup automático de RT

--- script.sh
+++ script.sh
@@ -7,5 +7,19 @@ sudo echo "deb http://10.79.1.71:3142/ftp.debian.org/debian/ wheezy main contrib
 sudo apt-get update
 
 # instalar paquetes
+sudo apt-get install debconf-utils
+
+# respondo las preguntas del instalador de manera automática
+sudo debconf-set-selections /vagrant/request-tracker4.seed
+
+# Instalar paquetes
 sudo apt-get -y install less screen git lsof vim postgresql
-sudo apg-get -y install request-tracker4  rt4-db-postgresql  rt4-fcgi
+sudo apt-get -y install rt4-db-postgresql rt4-apache2
+sudo apt-get -y install request-tracker4
+
+# Configuraciones
+rsync -av /vagrant/etc/ /etc/
+
+# Reinicio servicios
+/etc/init.d/apache2 stop
+/etc/init.d/apache2 start
